ae859ef302276096073eca8063c30f4dcd57ab21
EMERSON\Emerson <emersonline2007@gmail.com>
2017-03-24 01:49:51 -0300

    Revert "Aleração de Estrutura HTML para PHP."
    This reverts commit 055977b692fc2b7927aea4dc3a61e37769050b18.

--- includes/organisms/menu-responsivo.php
+++
@@ -1,8 +0,0 @@
-<nav class="menu-responsivo">
-	<ul class="li">
-		<li><a href="index.php">Início</a></li>
-		<li><a href="sobre.php">Sobre</a></li>
-		<li><a href="fotos.php">Fotos</a></li>
-		<li><a href="contato.php">Contato</a></li>
-	</ul>
-</nav>
